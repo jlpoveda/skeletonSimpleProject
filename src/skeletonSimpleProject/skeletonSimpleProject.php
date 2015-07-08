74ab2798149bf2df17236d0fc01162599ae20493
Jose Luis Poveda <joseluis.poveda@ofertix.com>
2015-07-08 12:29:15 +0200

    first commit

---
+++ src/skeletonSimpleProject/skeletonSimpleProject.php
@@ -0,0 +1,6 @@
+<?php
+namespace skeletonSimpleProject;
+
+class skeletonSimpleProject
+{
+}
---
+++ tests/skeletonSimpleProject/Tests/skeletonSimpleProjectTest.php
@@ -0,0 +1,9 @@
+<?php
+namespace skeletonSimpleProject\Tests;
+
+use PHPUnit_Framework_TestCase;
+use skeletonSimpleProject\skeletonSimpleProject;
+
+class skeletonSimpleProjectTest extends PHPUnit_Framework_TestCase
+{
+}
